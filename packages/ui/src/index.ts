3cf42509a1c84f81992c129cf594b36429baf354
xBinKai <fxia.pe@gmail.com>
2026-03-18 11:16:55 +0900

    feat(13.3-02): export shadcn/ui components from package index

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -6,3 +6,6 @@
 
 // Re-export the Hono app for use in CLI and other consumers
 export { default as app, type Env, type AppType } from "./server/index.js"
+
+// Re-export shadcn/ui components
+export * from "./components/ui/index.js"
